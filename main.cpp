365ccbe2dec649b590afa9465bd78c5cfc9036b9
sekzerd <sekzerd@outlook.com>
2024-08-10 12:12:04 +0800

    fix multiply thread error

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include <QCoreApplication>
+#include <QMutex>
 #include <QThread>
 #include <QtConcurrent>
 #include <QtDebug>
 
 void customMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
 {
+    static QMutex mutex;
+    mutex.lock();
     static QFile logFile("log.txt");
-    if (!logFile.isOpen())
+    if (!logFile.isOpen()) {
         logFile.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Text);
-
+    }
     QTextStream out(&logFile);
     QString buffer;
     switch (type) {
@@ -39,17 +42,36 @@ void customMessageHandler(QtMsgType type, const QMessageLogContext &context, con
     buffer += ") ";
     out << buffer << endl;
     qDebug() << buffer;
+    mutex.unlock();
 }
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     qInstallMessageHandler(customMessageHandler);
-
-    QtConcurrent::run([]() {
+    QThreadPool::globalInstance()->setMaxThreadCount(4);
+    QtConcurrent::run(QThreadPool::globalInstance(), []() {
         while (true) {
-            qDebug() << "running";
-            QThread::msleep(1000);
+            qDebug() << "running ID:" << QThread::currentThreadId();
+            QThread::msleep(1);
+        }
+    });
+    QtConcurrent::run(QThreadPool::globalInstance(), []() {
+        while (true) {
+            qDebug() << "running ID:" << QThread::currentThreadId();
+            QThread::msleep(1);
+        }
+    });
+    QtConcurrent::run(QThreadPool::globalInstance(), []() {
+        while (true) {
+            qDebug() << "running ID:" << QThread::currentThreadId();
+            QThread::msleep(1);
+        }
+    });
+    QtConcurrent::run(QThreadPool::globalInstance(), []() {
+        while (true) {
+            qDebug() << "running ID:" << QThread::currentThreadId();
+            QThread::msleep(1);
         }
     });
     return a.exec();
